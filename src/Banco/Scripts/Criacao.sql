9d79e120dc90691a1bda662e4d553d7c417e9acb
Joao Calado <joao1823@hotmail.com>
2020-04-26 00:24:38 -0300

    Incremento de script para criação da tabela parametrizacao.

--- src/Banco/Scripts/Criacao.sql
+++ src/Banco/Scripts/Criacao.sql
@@ -13,11 +13,30 @@ CREATE TABLE Usuarios (
 	CONSTRAINT pk_usuarios PRIMARY KEY (user_login)
 );
 
-create table cliente(
+create table cliente (
+    
     cli_cod SERIAL,
     cli_nome varchar(50) not null,
     cli_cpf varchar(14) not null,
     cli_email varchar(30) not null,
     cli_fone varchar(14) not null,
-    constraint pk_cli primary key (cli_cod)
+    constraint pk_cli primary key(cli_cod)
+);
+
+create table parametrizacao (
+    
+    par_cod SERIAL,
+    par_nome varchar(30) not null,
+    par_cep varchar(10) not null,
+    par_endereco varchar(50) not null,
+    par_cidade varchar(20) not null,
+    par_uf varchar(2) not null,
+    par_cnpj varchar(18) not null,
+    par_razao_social varchar(50) not null,
+    par_fone varchar(14) not null,
+    par_email varchar(30) not null,
+    par_cor1 varchar(7),
+    par_cor2 varchar(7),
+    par_icone bytea,
+    constraint pk_par primary key(par_cod)
 );
