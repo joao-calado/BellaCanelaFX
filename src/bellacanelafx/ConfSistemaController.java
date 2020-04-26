18dfcf2375831c2f0ed41f7878cc2f6d7520ba66
Joao Calado <joao1823@hotmail.com>
2020-04-26 20:32:44 -0300

    Finalização de várias verificações para salvar e alterar parametrização.

--- src/bellacanelafx/ConfSistemaController.java
+++ src/bellacanelafx/ConfSistemaController.java
@@ -6,13 +6,17 @@
 package bellacanelafx;
 
 import bellacanela.db.dal.DALConfSistema;
+import bellacanela.util.ConsultaAPI;
 import bellacanela.util.MaskFieldUtil;
 import bellacanelafx.db.entidades.ConfSistema;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXColorPicker;
 import com.jfoenix.controls.JFXTextField;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -21,16 +25,19 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.animation.FadeTransition;
+import javafx.concurrent.Task;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.util.Duration;
 import javax.imageio.ImageIO;
 
@@ -77,7 +84,6 @@ public class ConfSistemaController implements Initializable {
     
     private Image img;
     static public File arq = null;
-    static private int flag = 0;
     
     /**
      * Initializes the controller class.
@@ -96,16 +102,16 @@ public class ConfSistemaController implements Initializable {
         
         if((email.contains("@hotmail") || email.contains("@gmail")) && email.contains(".com"))
             res = true;
-        
-        return res;
-    }
-    
-    private boolean verificarCpf (String cpf) {
-        
-        boolean res = false;
-        
-        if(cpf.length() == 14)
-            res = true;
+        else {
+            Alert a = new Alert(Alert.AlertType.WARNING);
+            a.setHeaderText(null);
+            a.setTitle("Atenção:");
+            a.setContentText("Informe um email em formato válido!");
+            txemail.clear();
+            txemail.requestFocus();
+            a.showAndWait();
+        }
+       
         
         return res;
     }
@@ -116,6 +122,15 @@ public class ConfSistemaController implements Initializable {
         
         if(fone.length() >= 13)
             res = true;
+        else {
+            
+            Alert a = new Alert(Alert.AlertType.WARNING);
+            a.setTitle("Atenção:");
+            a.setContentText("Informe um fone em formato válido!");
+            txfone.clear();
+            txfone.requestFocus();
+            a.showAndWait();
+        }
         
         return res;
     }
@@ -167,35 +182,166 @@ public class ConfSistemaController implements Initializable {
     }    
 
     @FXML
-    private void clkBtSalvar(ActionEvent event) {
+    private void clkBtSalvar(ActionEvent event) throws FileNotFoundException {
+        
+        FileInputStream imagem;
+        DALConfSistema dal = new DALConfSistema();
+        ConfSistema cs = dal.get();
+        
+        if(verificarEmail(txemail.getText()) && verificarFone(txfone.getText())) {
+            
+            if(cs == null) { // significa que não tem parametros salvos, portanto é o primeiro acesso
+
+                cs = new ConfSistema(0, txnome.getText(), txcep.getText(), txendereco.getText(), txcidade.getText(), txuf.getText(), txcnpj.getText(), txrazao.getText(), txfone.getText(), txemail.getText(), cpcor1.getValue().toString(), cpcor2.getValue().toString());
+
+                if(dal.gravar(cs)) {
+
+                    if(arq == null) { // caso nenhum icone tenha sido selecionada
+
+                        try {
+
+                            File n = new File("src/imagens/icone_hamburguer.png");
+                            arq = n;
+                            imagem = new FileInputStream(arq);
+                            dal.gravarIcone(cs, imagem);
+                        }
+                        catch(SQLException ex) {}
+                    }
+                    else { // caso algum icone tenha sido selecionado
+
+                        try {
+
+                            imagem = new FileInputStream(arq);
+                            dal.gravarIcone(cs, imagem);
+                        }
+                        catch(SQLException ex) {}
+                    }
+                }
+            }
+            else { // significa que não é o primeiro acesso
+
+                cs = new ConfSistema(0, txnome.getText(), txcep.getText(), txendereco.getText(), txcidade.getText(), txuf.getText(), txcnpj.getText(), txrazao.getText(), txfone.getText(), txemail.getText(), cpcor1.getValue().toString(), cpcor2.getValue().toString());
+                if(dal.alterar(cs)) {
+
+                    if(arq == null) { // caso nenhum icone tenha sido selecionada
+
+                        try {
+
+                            File n = new File("src/imagens/icone_hamburguer.png");
+                            arq = n;
+                            imagem = new FileInputStream(arq);
+                            dal.gravarIcone(cs, imagem);
+                        }
+                        catch(SQLException ex) {}
+                    }
+                    else { // caso algum icone tenha sido selecionado
+
+                        try {
+
+                            imagem = new FileInputStream(arq);
+                            dal.gravarIcone(cs, imagem);
+                        }
+                        catch(SQLException ex) {}
+                    }
+                }
+            }
+        }    
+        
+        arq = null;
     }
 
     @FXML
     private void dgtNome(KeyEvent event) {
+        
+        if(txnome.getText().length() > 28) {
+            event.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
     }
 
     @FXML
     private void dgtEndereco(KeyEvent event) {
+        
+        if(txendereco.getText().length() > 48) {
+            event.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
     }
 
     @FXML
     private void dgtCidade(KeyEvent event) {
+        
+        if(txcidade.getText().length() > 18) {
+            event.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
     }
 
     @FXML
     private void dgtUF(KeyEvent event) {
+        
+        if(txuf.getText().length() > 1) {
+            event.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
     }
 
     @FXML
     private void dgtRazao(KeyEvent event) {
+        
+        if(txrazao.getText().length() > 48) {
+            event.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
     }
 
     @FXML
     private void dgtEmail(KeyEvent event) {
+        
+        if(txemail.getText().length() > 28) {
+            event.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
     }
 
     @FXML
     private void mcEscolherIcone(MouseEvent event) {
+        
+        FileChooser fc=new FileChooser();
+        fc.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("All Images","*.jpeg","*.png","*.jpg","*.png","*.gif"),
+            new FileChooser.ExtensionFilter("All Files","*.*"));
+        
+        arq = fc.showOpenDialog(null);
+        if (arq!=null) {
+
+           img = new Image(arq.toURI().toString());
+           imgIcone.setImage(img);
+        }
+    }
+
+    @FXML
+    private void dgtCEP(KeyEvent event) {
+        
+        if(txcep.getText().length() == 8 )
+        {
+            Task task = new Task() {
+                
+                @Override
+                protected Object call() throws Exception {
+                    
+                    String dados = ConsultaAPI.consultaCep(txcep.getText().replace("-", ""));
+                    /*JSONObject jason = new JSONObject(dados);
+                    
+                    txendereco.setText(jason.getString("logradouro"));
+                    txcidade.setText(jason.getString("localidade"));
+                    txuf.setText(jason.getString("uf"));
+                    
+                    */return null;
+                }
+            };
+            new Thread(task).start();
+        }
     }
     
 }
