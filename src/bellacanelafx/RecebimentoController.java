07f51bcbddab657ad30043eeac372a3f0ca13394
Joao Calado <joao1823@hotmail.com>
2020-05-23 18:19:21 -0300

    - carregando comandas no cbComandas da telas "fechar comandas".

--- src/bellacanelafx/RecebimentoController.java
+++ src/bellacanelafx/RecebimentoController.java
@@ -14,8 +14,8 @@ import com.jfoenix.controls.JFXComboBox;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.animation.FadeTransition;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -42,11 +42,11 @@ public class RecebimentoController implements Initializable {
     private Label lbTotal;
     @FXML
     private JFXComboBox<Comanda> cbComanda;
-    
+
     DALComanda dalCom = new DALComanda();
     DALProduto dalPro = new DALProduto();
     DALItensDaComanda dalItens = new DALItensDaComanda();
-
+    
     private void fadeout() {
         FadeTransition ft = new FadeTransition(Duration.millis(1000), painel);
         ft.setFromValue(0);
@@ -58,7 +58,7 @@ public class RecebimentoController implements Initializable {
         lbMesa.setText(" "+num);
     }
     
-    public void carregarCBComanda() {
+    public void carregarCBComandas() {
         
         cbComanda.setConverter(new StringConverter<Comanda>(){
             @Override
@@ -71,20 +71,32 @@ public class RecebimentoController implements Initializable {
                 throw new UnsupportedOperationException("Not supported yet.");
             }
         });
-        ObservableList<Comanda> olCom = FXCollections.observableArrayList(dalCom.getComandas("mes_cod = " + lbMesa.getText()));
-        cbComanda.setItems(olCom);
+        cbComanda.setItems(FXCollections.observableArrayList(new DALComanda().getComandas("mes_cod="+lbMesa.getText())));
         cbComanda.setValue(cbComanda.getItems().get(0));
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         fadeout();
-        carregarCBComanda();
-    }    
+        Platform.runLater(()->{
+            try{
+                carregarCBComandas();
+            }
+            catch(Exception e){
+                System.out.println("Erro!");
+            }
+        });
+        
+    }
 
     @FXML
     private void clkBtCancelar(ActionEvent event) {
         ((Stage)(btcancelar.getParent().getScene().getWindow())).close();
+    }
+
+    @FXML
+    private void clkCBComanda(ActionEvent event) {
+        System.out.println("oi");
     }
     
 }
