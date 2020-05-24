e63f977165b5ad5028e2d6ac3cbb362c85df4174
Joao Calado <joao1823@hotmail.com>
2020-05-23 22:16:08 -0300

    - valor total da comanda sendo exibido; - validação se a comanda possui cliente ou não; - carregando tabela com itens das comandas.

--- src/bellacanelafx/RecebimentoController.java
+++ src/bellacanelafx/RecebimentoController.java
@@ -1,26 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bellacanelafx;
 
 import bellacanela.db.dal.DALComanda;
 import bellacanela.db.dal.DALItensDaComanda;
 import bellacanela.db.dal.DALProduto;
 import bellacanelafx.db.entidades.Comanda;
+import bellacanelafx.db.entidades.ItensDaComanda;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.animation.FadeTransition;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.util.StringConverter;
@@ -31,6 +34,10 @@ import javafx.util.StringConverter;
  * @author joao
  */
 public class RecebimentoController implements Initializable {
+    
+    DALComanda dalCom = new DALComanda();
+    DALProduto dalPro = new DALProduto();
+    DALItensDaComanda dalItens = new DALItensDaComanda();
 
     @FXML
     private SplitPane painel;
@@ -42,10 +49,16 @@ public class RecebimentoController implements Initializable {
     private Label lbTotal;
     @FXML
     private JFXComboBox<Comanda> cbComanda;
-
-    DALComanda dalCom = new DALComanda();
-    DALProduto dalPro = new DALProduto();
-    DALItensDaComanda dalItens = new DALItensDaComanda();
+    @FXML
+    private TableColumn<ItensDaComanda, String> colProduto;
+    @FXML
+    private TableColumn<ItensDaComanda, Integer> colQtd;
+    @FXML
+    private TableColumn<ItensDaComanda, Double> colTotal;
+    @FXML
+    private TableView<ItensDaComanda> tabelaItens;
+    @FXML
+    private JFXComboBox<String> cbTipoRec;
     
     private void fadeout() {
         FadeTransition ft = new FadeTransition(Duration.millis(1000), painel);
@@ -75,18 +88,71 @@ public class RecebimentoController implements Initializable {
         cbComanda.setValue(cbComanda.getItems().get(0));
     }
     
+    public void carregarCBTipoRec() {
+        
+        if(!cbComanda.getValue().getCliente().getNome().equals("Outro")) {
+            
+            cbTipoRec.getItems().clear();
+            List<String> tr = new ArrayList();
+            tr.add("à vista");
+            tr.add("crédito");
+            tr.add("débito");
+            tr.add("a ver");
+            ObservableList<String> mod = FXCollections.observableArrayList(tr);
+            cbTipoRec.setItems(mod);
+            cbTipoRec.setValue(cbTipoRec.getItems().get(0));
+        }
+        else {
+               
+            System.out.println("aqui");
+            cbTipoRec.getItems().clear();
+            List<String> tr = new ArrayList();
+            tr.add("à vista");
+            tr.add("crédito");
+            tr.add("débito");
+            ObservableList<String> mod = FXCollections.observableArrayList(tr);
+            cbTipoRec.setItems(mod);
+            cbTipoRec.setValue(cbTipoRec.getItems().get(0));
+        }
+    }
+    
+    
+    public void carregarTabelaItens() {
+        
+        ObservableList<ItensDaComanda> olItens = FXCollections.observableArrayList(cbComanda.getSelectionModel().getSelectedItem().getItens());
+        tabelaItens.setItems(olItens);
+        tabelaItens.refresh();
+        
+        /*setar valor total da comanda*/
+        double tot = 0;
+        List<ItensDaComanda> itens = tabelaItens.getItems();
+        tot = itens.stream().map((idc) -> idc.getProduto().getPreco() * idc.getQtde()).reduce(tot, (accumulator, _item) -> accumulator + _item);
+        /* ou: 
+        for(ItensDaComanda idc : itens) {
+            tot += idc.getProduto().getPreco() * idc.getQtde();
+        }
+        */
+        lbTotal.setText(""+tot);
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
         fadeout();
+        colProduto.setCellValueFactory((TableColumn.CellDataFeatures<ItensDaComanda, String> p) -> new SimpleStringProperty(p.getValue().getProduto().getNome()));
+        colQtd.setCellValueFactory((TableColumn.CellDataFeatures<ItensDaComanda, Integer> p) -> new SimpleObjectProperty(p.getValue().getQtde()));
+        colTotal.setCellValueFactory((TableColumn.CellDataFeatures<ItensDaComanda, Double> p) -> new SimpleObjectProperty(p.getValue().getQtde()*p.getValue().getProduto().getPreco()));
+        
         Platform.runLater(()->{
             try{
                 carregarCBComandas();
+                carregarTabelaItens();
+                carregarCBTipoRec();
             }
             catch(Exception e){
                 System.out.println("Erro!");
             }
         });
-        
     }
 
     @FXML
@@ -96,7 +162,16 @@ public class RecebimentoController implements Initializable {
 
     @FXML
     private void clkCBComanda(ActionEvent event) {
-        System.out.println("oi");
+        
+        carregarTabelaItens();
+        if(cbComanda.getValue().getCliente().getNome().equals("Outro")) {
+            System.out.println(cbComanda.getValue().getCliente());
+            carregarCBTipoRec();
+        }
+    }
+
+    @FXML
+    private void clkCBTipoRec(ActionEvent event) {
     }
     
 }
